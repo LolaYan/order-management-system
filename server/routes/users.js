45a87972c3c238b26c2a2490460ed02365818b57
Lola Yan <lolayan@outlook.com>
2018-03-20 10:59:52 +1300

    POST new user data in DB

--- server/routes/users.js
+++ server/routes/users.js
@@ -14,4 +14,29 @@ router.get('/', function (req, res, next) {
 router.get('/new', function (req, res, next) {
   res.render('newUser');
 });
+
+function validUser(user) {
+  return user.name.trim() != '';
+}
+router.post('/', function (req, res, next) {
+  console.log(req.body);
+  if (validUser(req.body)) {
+    let user = {
+      name: req.body.name,
+      phone: req.body.phone,
+      email: req.body.email,
+      role: 'client'
+    };
+    knex('users')
+      .insert(user, 'user_id')
+      .then(ids => {
+        const id = ids[0];
+        res.redirect('/users')
+      });
+  } else {
+    res.status(500);
+    res.render('internal server error', { Message: 'invalid user' });
+  }
+});
+
 module.exports = router;
